1e3c5c28a08160f2971e35a0fabe9e1223edc106
unknown <hamidrezajafaei@outlook.com>
2025-07-05 01:21:55 +0330

    message app is done.

--- message/views.py
+++ message/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import message
 #def messageView(request):
 #    return render (request, 'home.html')
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = message
     template_name = 'home.html'
